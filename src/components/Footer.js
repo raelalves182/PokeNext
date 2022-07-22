169858071ee8d25e54305bdabc87ca3e97c5c9d1
Israel Alves <ysrael.2009@hotmail.com>
2022-07-21 23:31:53 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>
         <span>PokeNext</span> &copy; 2022
       </p>
