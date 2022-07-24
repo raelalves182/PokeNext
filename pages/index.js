414c0ed6253012fe9a8f753b2963d9242db079cf
Israel Alves <ysrael.2009@hotmail.com>
2022-07-24 20:38:18 -0300

    feat: css home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
+import Image from "next/image";
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251
+  const api = ' https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach(( item, index ) => {
+    item.id = index + 1
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({ pokemons }) {
   return (
-    <h1>Pokenext</h1>
+    <>
+      <div className="title">
+        <h1>Poke<span>Next</span></h1>
+        <Image 
+          src="/assets/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+
+      <div className="pokemons">
+        {pokemons.map((pokemon) => (
+          <p key={pokemon.id}>{pokemon.name}</p>
+        ))}
+      </div>
+    </>
   )
 }
